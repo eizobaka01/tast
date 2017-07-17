d6e31c34a8a96efcc9dd2c3aa836b98d579cb3b9
eizobaka01 <yusukebaka1120@gmail.com>
2017-07-18 00:51:58 +0900

    batAE

---
+++ batAE.jsx
@@ -0,0 +1,31 @@
+﻿var dlg = new Window("dialog","BL/batRender",[100,100,300,150]);
+dlg.Btn=dlg.add('button',[10,10,190,40], 'pushpush!');
+ var fileAE =new File ("C:/Users/eizobaka/Desktop/testAE/Ori.aep")
+dlg.Btn.onClick = function (){
+    alert("実行しますか？");
+    CreateBatAndExecute();
+    app.project.save(fileAE) ;
+    dlg.close();
+}
+ 
+function CreateBatAndExecute(){
+    var BatFile = new File("C:/Users/eizobaka/Desktop/testAE/"+"Ori"+".bat")
+    if (BatFile){
+        BatFile.open("w");
+        BatFile.writeln(BatCmd(BatFile));
+        BatFile.close();
+        //BatFile.execute()
+        }
+    else{
+        alert("batch file create failed")
+        }
+}
+ 
+
+
+function BatCmd(BatFile){
+    var Args = "testtest\r\ntest"
+    var CMD=Args
+    return CMD
+}
+dlg.show();
